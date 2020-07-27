18c8b8bff16230b5b28039e7884ef141094445c3
elthrasher <elthrasher@gmail.com>
2020-07-27 08:17:10 -0400

    deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,13 @@
 module.exports = {
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2019,
-    project: "./tsconfig.json",
-    sourceType: "module"
+    project: './tsconfig.json',
+    sourceType: 'module',
   },
   rules: {
-    "@typescript-eslint/interface-name-prefix": [
-      "error",
-      { prefixWithI: "always" }
-    ],
-    "@typescript-eslint/no-floating-promises": ["error"],
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }]
-  }
+    '@typescript-eslint/no-floating-promises': ['error'],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
 };
